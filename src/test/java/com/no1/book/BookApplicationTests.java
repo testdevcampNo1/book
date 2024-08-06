f3fbc891a679f6369291daa83b51812819eae777
HiSeungmin <osminosmin7180@gmail.com>
2024-08-06 20:50:46 +0900

    first commit

---
+++ src/test/java/com/no1/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.no1.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
