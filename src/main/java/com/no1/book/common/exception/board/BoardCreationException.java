8d15fc72765e69d4dfd6e8f6b05f427fb3dca342
KJU3 <kju_3@naver.com>
2024-08-17 15:22:10 +0900

    feat: 게시판 사용자 정의 예외 인터페이스 구현 및 하위 클래스 수정

--- src/main/java/com/no1/book/common/exception/board/BoardCreationException.java
+++ src/main/java/com/no1/book/common/exception/board/BoardCreationException.java
@@ -1,26 +1,17 @@
 package com.no1.book.common.exception.board;
 
+import com.no1.book.domain.board.BoardDto;
 import com.no1.book.domain.board.BoardNoticeDto;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 
-public class BoardCreationException extends DataIntegrityViolationException {
-    BoardNoticeDto boardNoticeDto;
-
+public class BoardCreationException extends BoardException {
     public BoardCreationException(String message) {
         super(message);
     }
 
-    public BoardCreationException(String message, BoardNoticeDto boardNoticeDto) {
-        super(message);
-        this.boardNoticeDto = boardNoticeDto;
-    }
-
-    public BoardCreationException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public BoardNoticeDto getBoardNoticeDto() {
-        return boardNoticeDto;
+    public BoardCreationException(String message, BoardDto boardDto) {
+        super(message, boardDto);
+        ;
     }
 }
--- src/main/java/com/no1/book/common/exception/board/BoardDeletionException.java
+++ src/main/java/com/no1/book/common/exception/board/BoardDeletionException.java
@@ -1,14 +1,18 @@
 package com.no1.book.common.exception.board;
 
+import com.no1.book.domain.board.BoardDto;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 
-public class BoardDeletionException extends DataAccessException {
+public class BoardDeletionException extends BoardException {
+
 
     public BoardDeletionException(String message) {
         super(message);
     }
-
+    public BoardDeletionException(String message, BoardDto boardDto){
+        super(message, boardDto);
+    }
     public BoardDeletionException(String message, Throwable cause) {
         super(message, cause);
     }
