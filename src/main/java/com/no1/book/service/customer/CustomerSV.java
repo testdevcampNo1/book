1af1f29839984f27deb3ab37604d4899d9494544
kendrickhama <gldjfh05723@gmail.com>
2024-08-18 21:03:29 +0900

    feat: 회원 구현 2차

--- src/main/java/com/no1/book/service/customer/CustomerSV.java
+++ src/main/java/com/no1/book/service/customer/CustomerSV.java
@@ -1,7 +1,12 @@
 package com.no1.book.service.customer;
 
 import com.no1.book.domain.customer.CustomerDto;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 import java.util.List;
 
@@ -11,8 +16,8 @@ public interface CustomerSV {
     public List<CustomerDto> getCustomerList();
     public CustomerDto getCustomerById(String custId);
     public CustomerDto selectCustomer(String custId,String pwd);
-    public void editInfo(String custId);
+    public void editInfo(CustomerDto customerDto);
     public boolean isIdAvailable(String custId);
     public boolean isLoggedIn(HttpSession session);
-
+    public MimeMessage CreateMail(String email);
 }
